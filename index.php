e6fbb458b6779dd0f47232e1454a18f01d33b69f
aki0akimichi <gaara0son@gmail.com>
2016-08-24 17:05:26 +0700

    clean code

--- index.php
+++ index.php
@@ -5,9 +5,11 @@ if(!isset($_SESSION))
 	$user=$_SESSION["user"];
 	$title="website kinh doanh máy tính xách tay";
 ?> 
+
 <?php include "connect.php"; include "function.php"; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title><?php
@@ -33,7 +35,8 @@ echo $title;
 	  <?php include "include/header.php";?>
   </div>
     <div id="menu-ngang">      
-	 <?php include "menu-top.php"; ?>   
+<!--clean code!>
+	<?php include "menu-top.php"; ?>   
     </div>
     <br clear="all" />
       
